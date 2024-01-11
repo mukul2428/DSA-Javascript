2c728fd5ea3fc150bfaceed8e3bb7acac77e1df6
Mukul Raghav <66879197+mukul2428@users.noreply.github.com>
2024-01-11 10:34:44 +0530

    more video content changes

---
+++ DSA Questions/outputBased.js
@@ -0,0 +1,22 @@
+// const nums = [1,2,3,4,5,6,7];
+// nums.forEach((n) => {
+//     if(n%2 === 0) 
+//       break;
+//     console.log(n);
+// });
+
+
+// function foo() {
+//     return "I'm the outer function";
+//   };
+  
+  
+//   function test() {
+//     console.log(bar);
+//     return foo();
+//     var bar = "I'm a variable";
+//     function foo() {
+//       return "I'm the inner function";
+//     }
+//   }
+//   console.log(test());
